227e09f5b8aac4613909b53b9ead4561ce7618af
hanchorong <hancrong@gmail.com>
2023-11-23 14:40:29 +0900

    education list animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// main greeting letter
 const $main_greeting = document.querySelectorAll(".main_greeting");
 
 $main_greeting.forEach(function (strip) {
@@ -20,18 +21,61 @@ $main_greeting.forEach(function (strip) {
   });
 });
 
-const spanElements = document.querySelectorAll(".main_greeting span:not(.row)");
-spanElements.forEach(function (span, index) {
-  setTimeout(function () {
-    span.classList.toggle("animate");
-  }, index * 15);
+// const spanElements = document.querySelectorAll(".main_greeting span:not(.row)");
+// spanElements.forEach(function (span, index) {
+//   setTimeout(function () {
+//     span.classList.toggle("animate");
+//   }, index * 15);
+// });
+
+// ---pull page
+
+const sectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const spanElements = entry.target.querySelectorAll(
+      ".main_greeting span:not(.row)"
+    );
+    const titles = entry.target.querySelectorAll(".title");
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add("in-view");
+
+      spanElements.forEach(function (span, index) {
+        setTimeout(function () {
+          span.classList.add("animate");
+        }, index * 15);
+      });
+
+      titles.forEach((title) => {
+        if (entry.target.hasChildNodes(".title")) {
+          title.classList.add("slideSide");
+        }
+      });
+    } else {
+      entry.target.classList.remove("in-view");
+
+      spanElements.forEach(function (span, index) {
+        setTimeout(function () {
+          span.classList.remove("animate");
+        }, index * 15);
+      });
+
+      titles.forEach((title) => {
+        title.classList.remove("slideSide");
+      });
+    }
+  });
+});
+
+const sections = document.querySelectorAll(".animated-section");
+
+sections.forEach((section) => {
+  sectionObserver.observe(section);
 });
 
 // about img slide
-
 const $slide_img_wrap = document.querySelector(".slide_img_wrap");
 const $slide_img = document.querySelectorAll(".slide_img_wrap img");
-// const lastImage = $slide_img[$slide_img.length - 1];
 
 let totalWidth = 0;
 
@@ -43,6 +87,20 @@ $slide_img.forEach((img) => {
 });
 $slide_img_wrap.style.width = `${totalWidth}px`;
 
-// let firstImg = $slide_img[0].cloneNode(true);
-// let firstImg = $slide_img.cloneNode(true);
-// $slide_img_wrap.appendChild(firstImg);
+// section - information education list
+const education_li = document.querySelectorAll(".education li");
+
+function scrollEvent() {
+  const scroll_Y = window.scrollY;
+
+  education_li.forEach((li) => {
+    console.log(li.offsetTop, window.innerHeight, scroll_Y);
+    if (li.offsetTop < window.innerHeight + scroll_Y - 200) {
+      console.log("Hmm");
+      li.style.backgroundPositionX = "0%";
+    } else {
+      li.style.backgroundPositionX = "100%";
+    }
+  });
+}
+window.addEventListener("scroll", scrollEvent);
